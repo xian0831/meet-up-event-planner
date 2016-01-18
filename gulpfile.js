41eee91f1819ad755cdb001f9fd0aa365208a21d
xian0831 <xian0831@gmail.com>
2016-01-17 19:26:59 -0500

    Basic file structure

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/*eslint-env node */
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', [], function() {
+    gulp.watch('./index.html')
+        .on('change',browserSync.reload);
+    browserSync.init({
+        server: './',
+        port: 8080
+    });
+});
