a241e44d305b9468224a159e21aa6f16cb1ae99b
xian0831 <xian0831@gmail.com>
2016-01-19 20:24:56 -0500

    add routing

--- app.js
+++ app.js
@@ -1,19 +1,76 @@
 var app = angular.module("meetUpApp", ["ngRoute"]);
 
+app.config(function($routeProvider){
+   $routeProvider.when("/",
+       {
+           templateUrl: "views/home.html",
+           controller: "AuthCtrl",
+           controllerAs: "auth"
+       })
+       .otherwise({
+           templateUrl: "views/home.html",
+           controller: "AuthCtrl",
+           controllerAs: "auth"
+       });
+});
 
+app.factory("users",function(){
+    var users =  {};
 
-// and use it in our controller
-app.controller("AuthCtrl", ["$scope", "Auth",
-    function($scope) {
-        $scope.createUser = function() {
-            $scope.message = null;
-            $scope.error = null;
+    users.list = [];
 
+    users.add = function(user) {
 
+        if(users.exist(user.email)){
+            return false;
+        } else {
+            users.list.push({"name": user.name,
+                "email": user.email,
+                "password": user.password});
+            return true;
+        }
+    };
+
+    users.exist = function (email) {
+        for (var i = 0; i < users.list.length; i++) {
+            if(users.list[i].email === email){
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    return users;
+});
+
+app.factory("event",function(){
+    var event = {};
+
+    event.list = [];
+
+});
+
+app.controller("AuthCtrl", function(users, $rootScope) {
+    var self = this;
+
+    self.users = users.list;
+
+    self.createUser = function() {
+        if(users.add({
+            "name": self.name,
+            "email": self.email,
+            "password": self.password
+        })){
+            $rootScope.currentUser = {
+                "name": self.name,
+                "email": self.email,
+                "password": self.password
+            };
         };
+    };
 
-        $scope.login = function() {
+    self.login = function() {
 
-        };
-    }
-]);
+    };
+});
