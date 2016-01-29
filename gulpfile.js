aeac62e27832c3e3e04a3b38512d853f420acf92
xian0831 <xian0831@gmail.com>
2016-01-29 15:43:06 -0500

    add gulp task for Sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
 /*eslint-env node */
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', [], function() {
+gulp.task('default', ['styles'], function() {
+    gulp.watch('sass/**/*.scss', ['styles']);
     gulp.watch('./*')
         .on('change',browserSync.reload);
-    gulp.watch('./views/*')
+    gulp.watch('./*/*')
         .on('change',browserSync.reload);
     browserSync.init({
         server: './',
         port: 8080
     });
+});
+
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
 });
