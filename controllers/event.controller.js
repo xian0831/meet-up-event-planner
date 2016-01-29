07fbbd66d1835d166506c1b5fc07753ff15145b0
xian0831 <xian0831@gmail.com>
2016-01-29 15:45:34 -0500

    add event type

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -8,18 +8,41 @@ EventCtrl.$inject = ["events","$log", "$location"];
 function EventCtrl(events, $log, $location) {
     var self = this;
 
+    self.eventAvailableType = [
+        "General",
+        "Birthday",
+        "Conference",
+        "Wedding",
+        "Party",
+        "Movie"
+    ];
+
+    self.eventType = self.eventAvailableType[0];
+
+    self.eventGuestList = [];
     self.createEvent = function() {
         events.add({
             "eventName" : self.eventName,
             "eventHost" : self.eventHost,
+            "eventType" : self.eventType,
             "eventLocation": self.eventLocation,
             "eventStartDate": self.eventStartDate,
             "eventStartTime": self.eventStartTime,
             "eventEndDate": self.eventEndDate,
-            "eventEndTime": self.eventEndTime
+            "eventEndTime": self.eventEndTime,
+            "eventGuestList": self.eventGuestList
         });
 
         $location.path("/list");
+    }
+
+    self.addGuest = function() {
+
+        if(self.eventGuest){
+            self.eventGuestList.push(self.eventGuest);
+            self.eventGuest = "";
+        }
+
     }
 
 
