eec79dbda0360c46353ccae364d7682d36b8bd0a
xian0831 <xian0831@gmail.com>
2016-02-03 22:01:49 -0500

    add copy-html task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
 /*eslint-env node */
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create();
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    browserSync = require("browser-sync").create();
 
-gulp.task('default', ['styles'], function() {
-    gulp.watch('sass/**/*.scss', ['styles']);
-    gulp.watch('./*')
-        .on('change',browserSync.reload);
-    gulp.watch('./*/*')
-        .on('change',browserSync.reload);
+gulp.task("default", ["copy-html","styles"], function() {
+    gulp.watch("sass/**/*.scss", ["styles"]);
+    gulp.watch("./*")
+        .on("change",browserSync.reload);
+    gulp.watch("./*/*")
+        .on("change",browserSync.reload);
     browserSync.init({
-        server: './',
+        server: "./",
         port: 8080
     });
 });
 
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+gulp.task("styles", function() {
+    gulp.src("sass/**/*.scss")
         .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
+            outputStyle: "compressed"
+        }).on("error", sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions']
+            browsers: ["last 2 versions"]
         }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
+});
+
+gulp.task("copy-html", function() {
+    gulp.src("./index.html")
+        .pipe(gulp.dest("./dist"));
 });
