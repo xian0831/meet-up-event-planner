f873880f83988b79d74dad00b6224a20c927cf76
xian0831 <xian0831@gmail.com>
2016-02-03 22:21:53 -0500

    add scripts task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,17 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
     browserSync = require("browser-sync").create();
+
+var jsOrder = [
+    "./app.js",
+    "services/**/*.service.js",
+    "controllers/**/*.controller.js",
+    "asset/*.js"
+];
 
 gulp.task("default", ["copy-html","styles"], function() {
     gulp.watch("sass/**/*.scss", ["styles"]);
@@ -26,6 +36,7 @@ gulp.task("styles", function() {
         .pipe(autoprefixer({
             browsers: ["last 2 versions"]
         }))
+        .pipe(gulp.dest('dist/css'))
         .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
@@ -33,4 +44,15 @@ gulp.task("styles", function() {
 gulp.task("copy-html", function() {
     gulp.src("./index.html")
         .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('scripts', function() {
+    gulp.src(jsOrder)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('js'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 });
